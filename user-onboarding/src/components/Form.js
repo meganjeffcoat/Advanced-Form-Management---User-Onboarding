c3e5baf41634a6e91c646637dc29b0840322d90f
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-08-15 00:24:49 -0500

    Form build out

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import { Form, Field, withFormik } from 'formik';
-import axios from 'axios';
+import React from "react";
+import { Form, Field, withFormik } from "formik";
+import axios from "axios";
 
-const UserForm = () => {
-    return (
-        <div>
-            <h1>Form</h1>
-        </div>
-    )
-}
+const UserForm = ({ values }) => {
+  return (
+    <div>
+      <h1>User Details</h1>
+      <Form>
+        <Field 
+            component="input" 
+            type="text" 
+            name="name" 
+            placeholder="Name" 
+        />
+        <Field 
+            component="input"
+            type="text"
+            name="email"
+            placeholder="Email"
+        />
+        <Field 
+            component="input"
+            type="text"
+            name="password"
+            placeholder="Password"
+        />
+        <label>
+            Agree to Terms of Service
+            <Field 
+                type="checkbox"
+                name="termsOfService"
+                checked={values.termsOfService}
+            />
+        </label>
+        <button>Submit</button>
+      </Form>
+    </div>
+  );
+};
 
+const formikHOC = withFormik({
+  mapPropsToValues({ name, email, password, termsOfService }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      termsOfService: termsOfService || false
+    };
+  }
+});
+const UserFormWithFormik = formikHOC(UserForm)
 
-
-export default UserForm;
+export default UserFormWithFormik;
